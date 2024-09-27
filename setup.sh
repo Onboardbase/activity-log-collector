c461dfa83441ddb4ff64bf5f7d710af566ba0f88
Aleem Isiaka <aleemisiaka@gmail.com>
2024-09-27 01:20:17 +0100

    make script path absolute

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ if [[ -z $2 ]]; then
 fi
 
 # Path to your Python script
-SCRIPT_PATH="ONBOARDBASE_PUBLIC_API_KEY=${1} ONBOARDBASE_PROJECT=${2} python $(dirname "${BASH_SOURCE[0]}")/fetch_onboardbase_logs.py"
+SCRIPT_NAME="$(dirname "${BASH_SOURCE[0]}")/fetch_onboardbase_logs.py"
+SCRIPT_PATH=$(readlink -f $SCRIPT_NAME)
 
-# Check if the script exists
 if [[ ! -f "$SCRIPT_PATH" ]]; then
     echo "Error: The script at $SCRIPT_PATH does not exist."
     exit 1
@@ -21,25 +21,22 @@ fi
 mkdir -p "/var/onboardbase/logs"
 mkdir -p "/etc/onboardbase/logs"
 
-sh -cx "$SCRIPT_PATH"
 
-# setup the agent
-# cat ossec.conf >> /etc/ossec/ossec.conf
 
-# # Create a temporary file to hold the current crontab
-# TEMP_CRONTAB=$(mktemp)
+ # Create a temporary file to hold the current crontab
+ TEMP_CRONTAB=$(mktemp)
 
-# # Get the current crontab
-# crontab -l > "$TEMP_CRONTAB"
+ # Get the current crontab
+ crontab -l > "$TEMP_CRONTAB"
 
-# # Add the new cron job (every 3 minutes)
-# # Adjust the command to activate your Python environment if necessary
-# echo "*/3 * * * * /usr/bin/python3 $SCRIPT_PATH" >> "$TEMP_CRONTAB"
+ # Add the new cron job (every 3 minutes)
+ # Adjust the command to activate your Python environment if necessary
+ echo "*/3 * * * * ONBOARDBASE_PUBLIC_API_KEY=${1} ONBOARDBASE_PROJECT=${2} /usr/bin/python3 $SCRIPT_PATH >> /var/onboardbase/logs/cron.log 2>&1" >> "$TEMP_CRONTAB"
 
-# # Install the new crontab
-# crontab "$TEMP_CRONTAB"
+ # Install the new crontab
+ crontab "$TEMP_CRONTAB"
 
-# # Remove the temporary file
-# rm "$TEMP_CRONTAB"
+ # Remove the temporary file
+ rm "$TEMP_CRONTAB"
 
-# echo "Cron job added: Run $SCRIPT_PATH every 3 minutes."
+ echo "Cron job added: Run $SCRIPT_PATH every 3 minutes."
